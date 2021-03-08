5e36167a5e66358bcc680170c91ca44142226418
Tech-Nayuta <ketanayutawork@gmail.com>
2021-03-09 00:34:51 +0900

    Podsのキャッシュ

--- Pods/Target Support Files/Pods-hack_iOS-hack_iOSUITests/Pods-hack_iOS-hack_iOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_hack_iOS_hack_iOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_hack_iOS_hack_iOSUITests
-@end
--- Pods/Target Support Files/Pods-hack_iOS/Pods-hack_iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_hack_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_hack_iOS
-@end
