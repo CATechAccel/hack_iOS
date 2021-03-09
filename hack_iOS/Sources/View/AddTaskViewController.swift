c2dd8ea8ebd378f3f8286cf48e48a8a94fa77daa
yamanetaisei <swiftmantaisei@gmail.com>
2021-03-10 01:37:20 +0900

    AddTaskVCにUIパーツを配置

--- hack_iOS/Sources/View/AddTaskViewController.swift
+++ hack_iOS/Sources/View/AddTaskViewController.swift
@@ -8,8 +8,29 @@
 import UIKit
 
 class AddTaskViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputnameTextField: UITextField! {
+        didSet {
+            inputnameTextField.placeholder = "タスクのなまえ"
+        }
+    }
+    @IBOutlet weak var inputDescriptionTextView: UITextView! {
+        didSet {
+            inputDescriptionTextView.isScrollEnabled = false
+        }
+    }
+    @IBOutlet weak var addButton: UIButton! {
+        didSet {
+            addButton.setTitle("Add", for: .normal)
+            addButton.addTarget(self, action: #selector(tapAddButton), for: .touchUpInside)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @objc private func tapAddButton() {
+        self.navigationController?.popViewController(animated: true)
     }
 }
