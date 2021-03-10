2e78ec9abca2d160494f91f78c52f6c548a98aad
yamanetaisei <swiftmantaisei@gmail.com>
2021-03-10 16:53:02 +0900

    Decode用のModelを定義

---
+++ hack_iOS/Sources/Model/Task.swift
@@ -0,0 +1,15 @@
+//
+//  TaskModel.swift
+//  hack_iOS
+//
+//  Created by 山根大生 on 2021/03/10.
+//
+
+import Foundation
+// TODO どのステータスがoptionalになるかなどについてサーバーチームと確認し、応じて修正する必要あり
+struct Task: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let done: Bool
+}
