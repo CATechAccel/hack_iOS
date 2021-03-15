f59a62183adbe2f58ab493c04b72fb17b3064b8c
Tech-Nayuta <ketanayutawork@gmail.com>
2021-03-15 09:57:41 +0900

    main.yml

--- hack_iOS/Sources/SceneDelegate.swift
+++ hack_iOS/Sources/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        window.rootViewController =　UINavigationController(rootViewController:  LoginViewController())
+        window.rootViewController = UINavigationController(rootViewController:  LoginViewController())
         window.makeKeyAndVisible()
     }
 
