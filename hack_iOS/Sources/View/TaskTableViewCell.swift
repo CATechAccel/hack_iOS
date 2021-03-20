c8ec6ce032f5867c2ec47f9cacc16c81835ede8f
yamanetaisei <swiftmantaisei@gmail.com>
2021-03-20 23:51:51 +0900

    DoneのAPI処理

--- hack_iOS/Sources/View/TaskTableViewCell.swift
+++ hack_iOS/Sources/View/TaskTableViewCell.swift
@@ -20,11 +20,41 @@ final class TaskTableViewCell: UITableViewCell {
         didSet {
             // TODO: bool値を参照して、画像を変更できるようにする
             doneButton.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
+            doneButton.addTarget(self, action: #selector(tapDoneButton), for: .touchUpInside)
         }
     }
+
+    private var taskId: String = ""
+    let taskRepository = TaskRepository()
     
     func configure(with task: Task) {
+        taskId = task.id
         nameLabel.text = task.name
         descriptionTextView.text = task.description
+    }
+    
+    
+    @objc private func tapDoneButton() {
+        done()
+    }
+    
+    private func done() {
+        taskRepository.done(id: taskId, completion: { result in
+            switch result {
+            case .success(()):
+                print("success")
+            case .failure(let error):
+                switch error {
+                case .decode(let error):
+                    print(error)
+                case .network(let error):
+                    print(error)
+                case .unknown(let error):
+                    print(error)
+                case .noResponse:
+                    print("No Response")
+                }
+            }
+        })
     }
 }
